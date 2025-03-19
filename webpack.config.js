72f57f4a802cb6bfedd5931d8478b4d500f0a1a4
evan.loria4 <evan.loria4@gmail.com>
2025-03-19 09:25:06 -0500

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,84 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
+const CLIENT_DIR = path.resolve(__dirname, 'src', 'client');
+
 module.exports = {
-  
+  mode: 'development',
+  entry: path.resolve(CLIENT_DIR, 'index.tsx'),
+  output: {
+    path: DIST_DIR,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(m?js)$/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|mp3|aac|ogg|ico)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-typescript',
+                '@babel/preset-react',
+              ]
+            }
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          'postcss-loader'
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(CLIENT_DIR, 'index.html'),
+      filename: 'index.html',
+      inject: true
+    })
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css'],
+    alias: {
+      '@': SRC_DIR,
+    },
+    modules: [
+      'node_modules',
+      SRC_DIR,
+    ],
+    fallback: {
+      "crypto": false
+    }
+  },
 }
