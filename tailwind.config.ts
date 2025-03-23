201148b1b960e09b150d3b724946e7e94aed314f
evan.loria4 <evan.loria4@gmail.com>
2025-03-23 14:42:05 -0500

    Setup tailwind.config with postcss and main.css files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,html}', './src/styles/main.css'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
   theme: {
     extend: {
       colors: {
