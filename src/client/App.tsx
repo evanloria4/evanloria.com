033a3ad94796ee27f36c36cfc8aac720069aafa7
evan.loria4 <evan.loria4@gmail.com>
2025-03-23 22:59:29 -0500

    Style header over portfolio

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,25 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/main.css';
-import profile from './img/profile.png';
+import React, { useState, useEffect } from "react";
+import "../styles/main.css";
+import profile from "./img/profile.png";
 
 function App() {
   return (
-    <section className="bg-primary text-white px-6 py-16">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-12">
-        <div className="flex flex-col items-center md:items-start text-center md:text-left gap-6 max-w-md">
-          <img
-            src={profile}
-            alt="Hello"
-            className="w-40 h-40 rounded-full border-4 border-white"
-          />
-          <h1 className="text-4xl font-bold">Software Developer</h1>
-          <button className="bg-accent text-white px-6 py-2 rounded-lg hover:bg-opacity-90 transition">
-            About Me
-          </button>
+    <div className="min-h-screen flex flex-col">
+      <section className="bg-primary text-white pt-6">
+        <div className="mx-auto flex flex-col items-center justify-between gap-8">
+          <div className="flex flex-col items-center text-center gap-6 max-w-md">
+            <img
+              src={profile}
+              alt="Hello"
+              className="w-40 h-40 rounded-full border-4 border-white"
+            />
+            <div className="flex-cil gap-2">
+              <div className="text-4xl font-bold">Evan Loria</div>
+              <div className="text-2xl font-bold">Software Developer</div>
+            </div>
+            <p>
+              React | Node.js | JavaScript | Typescript | Express | Axios | SQL
+              | NoSQL | AWS | tailwindcss | webpack | Rest APIs | HTML
+            </p>
+          </div>
+          <div className="w-full md:w-1/2"></div>
         </div>
-        <div className="w-full md:w-1/2"></div>
-      </div>
-    </section>
+      </section>
+      <div className="bg-cream flex-1 text-white"></div>
+    </div>
   );
 }
 
