c5e86932566be4b9e790a625cebbdede50b4c47b
evan.loria4 <evan.loria4@gmail.com>
2025-03-25 12:28:15 -0500

    Add app.get() to server/app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -5,4 +5,8 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../dist/index.html'));
+});
+
 export default app;
