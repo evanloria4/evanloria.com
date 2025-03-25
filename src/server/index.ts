c959a85f96572fe00fa2d38fe78058bd22d5a24d
evan.loria4 <evan.loria4@gmail.com>
2025-03-25 11:33:33 -0500

    Change console.log in app.listen

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,5 +7,5 @@ dotenv.config();
 const PORT = parseInt(process.env.PORT || '4000')
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
